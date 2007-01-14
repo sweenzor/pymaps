661e900f86ea18da9c5ee14c45bd11915d6e1799
Ashley Camba Garrido <ashwoods@gmail.com>
2007-01-14 22:25:20 +0000

    added html window overlays :)

--- pymaps.py
+++ pymaps.py
@@ -41,57 +41,99 @@
 
 
 
-"""
+"""   
+        
 
 
-
-
-class Pymap:
+class PyMap:
     """
     Python wrapper class for Google Maps API.
-    
-    
-    
+            
     """
     
+    class Point:
+        pass
     
     def __init__(self, key=None):
-        """
-        Load default values
+        """ Default values """
         
-        """
-        self.key      = key
-        self.width    = "500px"
-        self.height   = "300px"
-        self.centerlat   = 0
-        self.centerlong  = 0
-        self.zoom        = "1"
-        self.controls    = True
-        self.points = []
+        self.id       = "map"    # div id
+        self.key      = key      # set your google key
+        self.width    = "500px"  # map div width
+        self.height   = "300px"  # map div height
+        self.centerlat   = 0     # center map latitute coordinate
+        self.centerlong  = 0     # center map longitud coordinate
+        self.zoom        = "1"   # zoom level
+        self.controls    = True  # show google map controls
+        self.points = []         # point list
     
-    def SetPoint(self, point):
+    def setpoint(self, point):
+        """ Add a point (lat,long) """
+        
         self.points.append(point)
     
-    def ControlJS(self):
-            if self.controls:
-                return "map.addControl(new GSmallMapControl());\n map.addControl(new GMapTypeControl());"
-            else:
-                return ""    
+    def mapcontrol(self):
+        """ Returns the javascript for google maps control"""    
+        if self.controls:
+            return "map.addControl(new GSmallMapControl());\n map.addControl(new GMapTypeControl());"
+        else:
+            return ""    
     
-    def RenderPoints(self):
-            if self.points <> []: 
-                self.pointjs = """var points = [];
-                %s
-                for (var i = 0; i < %s; i++) {
-                    map.addOverlay(new GMarker(points[i]));
-                }""" % ("".join(["\n                points.push(new GLatLng(%s, %s));" % (k, v) for k, v in self.points]), len(self.points))
-            else:
-                self.pointjs = ""
-            return self.pointjs
-            
+    def renderpoints(self):
+        """ Returns the javascript for all the points in self.points """
         
+        if self.points <> []: 
+            self.pointjs = """
+           function createMarker(point, html) {
+               var marker = new GMarker(point);
+               GEvent.addListener(marker, "click", function() {
+               marker.openInfoWindowHtml(html);
+               });
+               return marker;
+            }  
+            
+            var points = [];
+            %s
+            for (var i = 0; i < %s; i++) {
+            map.addOverlay(new GMarker(points[i]));
+            }
+            
+            var pointswithhtml = [];
+            var html = [];
+            %s
+            for (var i = 0; i < %s; i++) {
+            map.addOverlay(createMarker(pointswithhtml[i],html[i]));
+            }            
+            
+            
+            
+            """ % ("".join(["\n points.push(new GLatLng(%s, %s));" % (k[0], k[1]) for k in self.points if len(k) == 2]), len(self.points),"".join(["\n points.push(new GLatLng(%s, %s)); html.push('%s');" % (k[0], k[1], k[2]) for k in self.points if len(k) == 3]),len(self.points))
 
-    def MapJS(self):
+        else:
+            self.pointjs = ""
+        return self.pointjs
+
+    def showdiv(self):
+        """ Returns html for dislaying map """
+        
+        self.script = """\n<div id=\"%s\">\n</div>\n""" % (self.id)
+        return self.script
+    
+    def markerjs(self): 
+        """ Returns javascript for marker windows """
+        
+        self.markerjs =   """
+          function createMarker(point, number) {
+          var marker = new GMarker(point);
+          GEvent.addListener(marker, "click", function() {
+            marker.openInfoWindowHtml("Marker #<b>" + number + "</b>");
+          });
+          return marker;
+        }
+	
+    """
+    def mapjs(self):
+        """ Returns complete javacript for rendering map """
         
         self.js = """\n<script src=\"http://maps.google.com/maps?file=api&amp;v=2&amp;key=%s\" type="text/javascript"></script>
         <script type="text/javascript">
@@ -110,34 +152,36 @@ class Pymap:
         </script>
         
         
-        """ % (self.key, self.centerlat, self.centerlong, self.zoom, self.RenderPoints(), self.ControlJS())
-        return self.js
+        """ % (self.key, self.centerlat, self.centerlong, self.zoom, self.renderpoints(), self.mapcontrol())
+        return self.js    
+    
+    
         
+    def showhtml(self):
+        """returns a complete html page with a map"""
+        
+        self.html = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <title></title>
+    %s
+  </head>
 
-    def ShowMap(self):
-        
-        self.script = """\n<div id=\"map\">\n</div>\n
-        
-        
-        """ 
-        return self.script
-    
-    def MarkerJS(self): 
-        self.markerjs =   """
-          function createMarker(point, number) {
-          var marker = new GMarker(point);
-          GEvent.addListener(marker, "click", function() {
-            marker.openInfoWindowHtml("Marker #<b>" + number + "</b>");
-          });
-          return marker;
-        }
-		"""
-        
-    
+  <body onload="load()" onunload="GUnload()">
+    <div id="map" style="width: 500px; height: 300px"></div>
+  </body>
+</html>
+""" % (self.mapjs)
+        return self.html
+
 
 if __name__ == "__main__":
-    g = Pymap()
-    print g.ShowMap()
-    print g.MapJS()
-        
+    g = PyMap()
+    p = (1,1)
+    g.setpoint(p)
+    print g.showhtml()
+
             
