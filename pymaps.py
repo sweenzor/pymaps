d28379a0fa4f81a394a096034e73cf628dc72aa6
Ashley Camba Garrido <ashwoods@gmail.com>
2007-04-11 09:18:49 +0000

    major code modifications, multiple map support, added icon support, simplified javascript, app not tested! (might be broken)

--- pymaps.py
+++ pymaps.py
@@ -43,7 +43,24 @@
 
 """   
         
-
+class Map:
+    def __init__(self,id="map"):
+        self.id       = id    # div id        
+        self.width    = "500px"  # map div width
+        self.height   = "300px"  # map div height
+        self.center   = (0,0)     # center map latitute coordinate
+        self.zoom        = "1"   # zoom level
+        self.navcontrols  =   True   # show google map navigation controls
+        self.mapcontrols  =   True   # show toogle map type (sat/map/hybrid) controls
+        self.points = []         # point list
+    
+    def __str__(self):
+        return self.id
+        
+    
+    def setpoint(self, point):
+        """ Add a point (lat,long) """
+        self.points.append(point)
 
 class PyMap:
     """
@@ -51,79 +68,68 @@ class PyMap:
             
     """
     
+    def __str__(self):
+        return "Pymap"
     
-    def __init__(self, key=None):
+    def __init__(self, key=None, maplist=[Map(),]):
         """ Default values """
-        
-        self.id       = "map"    # div id
         self.key      = key      # set your google key
-        self.width    = "500px"  # map div width
-        self.height   = "300px"  # map div height
-        self.centerlat   = 0     # center map latitute coordinate
-        self.centerlong  = 0     # center map longitud coordinate
-        self.zoom        = "1"   # zoom level
-        self.controls    = True  # show google map controls
-        self.points = []         # point list
+        self.maps     = maplist
     
-    def setpoint(self, point):
-        """ Add a point (lat,long) """
-        
-        self.points.append(point)
     
-    def mapcontrol(self):
+    
+    def _navcontroljs(self,map):
         """ Returns the javascript for google maps control"""    
-        if self.controls:
-            return "map.addControl(new GSmallMapControl());\n map.addControl(new GMapTypeControl());"
+        if map.navcontrols:
+            return "%s.addControl(new GSmallMapControl());\n" % (map.id)
         else:
             return ""    
     
-    def renderpoints(self):
-        """ Returns the javascript for all the points in self.points """
-        
-        if self.points <> []: 
-            self.pointjs = """
-           function createMarker(point, html) {
-               var marker = new GMarker(point);
-               GEvent.addListener(marker, "click", function() {
-               marker.openInfoWindowHtml(html);
-               });
-               return marker;
-            }  
-            
-            var points = [];
-            %s
-            for (var i = 0; i < %s; i++) {
-            map.addOverlay(new GMarker(points[i]));
-            }
-            
-            var points_with_html = [];
-            var html = [];
-            %s
-            for (var i = 0; i < %s; i++) {
-               map.addOverlay(createMarker(points_with_html[i],html[i]));
-            }            
-            
-            
-            
-            """ % ("".join(["\n points.push(new GLatLng(%s, %s));" % (k[0], k[1]) for k in self.points if len(k) == 2]), \
-            len([v for v in self.points if len(v) == 2]),\
-            "".join(["\n points_with_html.push(new GLatLng(%s, %s)); html.push('%s');" % (k[0], k[1], k[2]) for k in self.points if len(k) == 3]),\
-            len([v for v in self.points if len(v) ==3]))
-
-        else:
-            self.pointjs = ""
-        return self.pointjs
-
-    def showdiv(self):
-        """ Returns html for dislaying map """
-        
-        self.script = """\n<div id=\"%s\">\n</div>\n""" % (self.id)
-        return self.script
     
+    def _mapcontroljs(self,map):
+        """ Returns the javascript for google maps control"""    
+        if map.controls:
+            return "%s.addControl(new GMapTypeControl());\n" % (map.id)
+        else:
+            return ""     
+    
+    def _renderpointsjs(self,map):
+        
+        
+        js = """   
+        var %s_points_with_html = [];
+        var %s_points = [];    
+        %s
+        %s
+        
+        """ % (map.id, map.id, "".join(["\n %s_points.push(new GLatLng(%s, %s));" % (map.id,k[0], k[1]) for k in map.points if len(k) == 2]), "".join(["\n %s_points_with_html.push(new GLatLng(%s, %s)); %s_html.push('%s');" % (map.id,k[0], k[1],map.id, k[2]) for k in map.points if len(k) == 3]) )
+        #% (map.id, map.id,"".join(["\n %s_points.push(new GLatLng(%s, %s));" % (map.id,k[0], k[1]) for k in map.points if len(k) == 2]), \
+        #    len([v for v in map.points if len(v) == 2]),\
+        #    "".join(["\n %s_points_with_html.push(new GLatLng(%s, %s)); %s_html.push('%s');" % (map.id,k[0], k[1],map.id, k[2]) for k in map.points if len(k) == 3]),\
+        #    len([v for v in map.points if len(v) ==3]))
+
+        return js
+
+    def _showdivhtml(self,map):
+        """ Returns html for dislaying map """
+        html = """\n<div id=\"%s\">\n</div>\n""" % (map.id)
+        return html
+    
+    
+    
+    def _mapjs(self,map):
+        mapjs = """
+        %s_points = %s;
+        var %s = new Map('%s',%s_points,%s,%s,%s);
+        """ % (map.id,map.points,map.id,map.id,map.id,map.center[0],map.center[1],map.zoom)
+        return mapjs
    
+    def _buildmaps(self):
+        for i in self.maps:
+            js = self._mapjs(i)
+        return js
 
-
-    def mapjs(self):
+    def pymapjs(self):
         """ Returns complete javacript for rendering map """
         
         self.js = """\n<script src=\"http://maps.google.com/maps?file=api&amp;v=2&amp;key=%s\" type="text/javascript"></script>
@@ -131,20 +137,60 @@ class PyMap:
         //<![CDATA[
         function load() {
             if (GBrowserIsCompatible()) {
-                var map = new GMap2(document.getElementById("map"));
-                map.setCenter(new GLatLng(%s, %s), %s);
                 
-                %s
+            
+            function Point(lat,long,html,icon) {
+                  this.gpoint = new GMarker(new GLatLng(lat,long),icon);
+                  this.html = html;
+                  
+               }               
+               
+               
+               function Map(id,points,lat,long,zoom) {
+                  this.id = id;
+                  this.points = points;
+                  this.gmap = new GMap2(document.getElementById(this.id));
+                  this.gmap.setCenter(new GLatLng(lat, long), zoom);
+                  this.markerlist = markerlist;
+                  this.addmarker = addmarker;
+                  this.array2points = array2points;
+                   
+                  function markerlist(array) {
+                     for (var i in array) {
+                        this.addmarker(array[i]);
+                     }
+                  }
+                  
+                  function array2points(map_points) {            
+              for (var i in map_points) {  
+                points[i] = new Point(map_points[i][0],map_points[i][1],map_points[i][2],map_points[i][3]);         }
+              return points;   
+            }                  
+                  
+                  function addmarker(point) {
+                     if (point.html) {
+                       GEvent.addListener(point.gpoint, "click", function() {
+                           point.gpoint.openInfoWindowHtml(point.html);
+                       });
+                     }
+                     this.gmap.addOverlay(point.gpoint);  
+                  }
+                  
+                  this.points = array2points(this.points);
+                  this.markerlist(this.points); 
+            }  
                 
-                %s
+            %s
+                
+               
             }
         }
         //]]>
         </script>
         
         
-        """ % (self.key, self.centerlat, self.centerlong, self.zoom, self.renderpoints(), self.mapcontrol())
-        return self.js    
+        """ % (self.key, self._buildmaps())
+        return self.js 
     
     
         
@@ -165,14 +211,19 @@ class PyMap:
     <div id="map" style="width: 500px; height: 300px"></div>
   </body>
 </html>
-""" % (self.mapjs())
+""" % (self.pymapjs())
         return self.html
 
 
 if __name__ == "__main__":
+        
     g = PyMap()
-    p = (1,1)
-    g.setpoint(p)
+    g.key = "ABQIAAAAQQRAsOk3uqvy3Hwwo4CclBTrVPfEE8Ms0qPwyRfPn-DOTlpaLBTvTHRCdf2V6KbzW7PZFYLT8wFD0A"    
+    p = [1,1]
+    s = [2,4,'hello']
+    g.maps[0].setpoint(p)
+    g.maps[0].setpoint(s)
     print g.showhtml()
-
+    
+    
             
